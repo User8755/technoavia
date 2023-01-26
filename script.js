35ad5ae18564eef82093f9f982da9eb04571f68c
Evgeny Kalinin <user8755@yandex.ru>
2023-01-26 08:25:03 +0500

    test v 0.0.1

--- script.js
+++ script.js
@@ -4,24 +4,20 @@ const article = document.querySelector(".block__paragraf_type-article");
 const input = document.querySelector(".input");
 const place = document.querySelector(".block__paragraf_type-place");
 
-const arr = [
-  { art: "111", place: "1-1-1" },
-  { art: "222", place: "5-2-1" },
-  { art: "333", place: "3-3-9" },
-  { art: "00230", place: "30-3-9" },
+const list = [
+  { art: "5149", place: "1-1-1" },
+  { art: "5148", place: "5-2-1" },
+  { art: "5147", place: "3-3-9" },
+  { art: "5676", place: "30-3-9" },
   { art: "5331", place: "31-4-1, 31-5-1" }
 ];
 
 const formSubmit = (evt) => {
   evt.preventDefault();
 };
-form.addEventListener("submit", formSubmit);
-button.addEventListener("click", () => {
-  (article.textContent = input.value), pre(findArt());
-});
 
-const findArt = () => {
-  const findArt = arr.find((item) => item.art === input.value);
+const findArt = (list) => {
+  const findArt = list.find((item) => item.art === input.value);
   return findArt;
 };
 
@@ -29,6 +25,11 @@ const pre = (item) => {
   if (typeof item == "object") {
     place.textContent = item.place;
   } else {
-    place.textContent = "Не верный артикул";
+    place.textContent = "Неверный артикул";
   }
 };
+
+form.addEventListener("submit", formSubmit);
+button.addEventListener("click", () => {
+  (article.textContent = input.value), pre(findArt(list), input.value = '');
+});
