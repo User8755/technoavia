8ac04f2d33bb08307112a074407b80bc08e9ebca
Evgeny Kalinin <user8755@yandex.ru>
2023-02-09 22:41:20 +0500

    add BD

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const article = document.querySelector(".block__paragraf_type-article");
 const input = document.querySelector(".input");
 const place = document.querySelector(".block__paragraf_type-place");
 
-const list = [
-  { art: "5149", place: "1-1-1" },
-  { art: "5148", place: "5-2-1" },
-  { art: "5147", place: "3-3-9" },
-  { art: "5676", place: "30-3-9" },
-  { art: "5331", place: "31-4-1, 31-5-1" }
-];
+// const list = [
+//   { art: "5149", place: "1-1-1" },
+//   { art: "5148", place: "5-2-1" },
+//   { art: "5147", place: "3-3-9" },
+//   { art: "5676", place: "30-3-9" },
+//   { art: "5331", place: "31-4-1, 31-5-1" }
+// ];
 
 const formSubmit = (evt) => {
   evt.preventDefault();
