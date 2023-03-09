301e9f7bb4967ac13ad1bf37bb27f1e9f374dffb
Evgeny Kalinin <user8755@yandex.ru>
2023-03-09 23:59:18 +0500

    test bild

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 const button = document.querySelector(".button");
 const form = document.querySelector(".form");
-const article = document.querySelector(".block__paragraf_type-article");
+const article = document.querySelector(".block__span_type-article");
 const input = document.querySelector(".input");
-const place = document.querySelector(".block__paragraf_type-place");
+const place = document.querySelector(".block__span_type-place");
 const times = document.querySelector(".header__time");
-
+const name = document.querySelector(".block__span_type-name");
 const time = new Date();
-const min = time.getMinutes();
-
-const minutes = () => {
-  if (`${min}`.length > 1) {
-    return min;
-  } else {
-    return 0 + `${min}`;
-  }
-};
 
 const setTimes = () => {
   const data = new Date();
   const min = data.getMinutes();
-  const hours = data.getHours()
+  const hours = data.getHours();
 
   const time = (item) => {
-   return `0${item}`.slice(-2)
-   }
+    return `0${item}`.slice(-2);
+  };
 
   times.textContent = `${time(hours)} : ${time(min)}`;
 };
@@ -34,28 +25,41 @@ const formSubmit = (evt) => {
   evt.preventDefault();
 };
 
-const findArt = (list) => {
-  const foundArt = [];
+const findObj = (list) => {
   const findObj = list.filter((item) => item.art === input.value);
-  findObj.forEach((item) => {
-    foundArt.push(item.place);
-  });
 
-  return foundArt;
+  return findObj;
 };
+
+const findPlace = (item) => {
+  const foundArt = [];
+  item.forEach((el) => {
+      foundArt.push(el.place);
+     });
+
+     return foundArt
+}
+
+const findName = (item) => {
+  const foundName = [];
+  foundName.push(item[0].name)
+
+  return foundName
+}
 
 const pre = (item) => {
   if (item.length > 0) {
-    const arrr = item.join("\n");
-    console.log(arrr);
+    const arrr = findPlace(item).join("\n");
     const arrrr = arrr.split(", <br>");
     place.textContent = arrrr;
+    name.textContent = findName(item)
   } else {
     place.textContent = "Место не задано";
+    name.textContent = ''
   }
 };
 
 form.addEventListener("submit", formSubmit);
 button.addEventListener("click", () => {
-  (article.textContent = input.value), pre(findArt(list), (input.value = ""));
+  (article.textContent = input.value), pre(findObj(list), input.value = "");
 });
